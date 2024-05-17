e0057afdc2e859ddb025a1b6e4c2df16bbe1bb0a
Дмитрий <sulagaevdim@mail.ru>
2024-05-17 16:10:09 +0400

    delete DAO

--- src/notebook/Main.java
+++ src/notebook/Main.java
@@ -1,7 +1,6 @@
 package notebook;
 
 import notebook.controller.UserController;
-import notebook.model.dao.impl.FileOperation;
 import notebook.model.repository.GBRepository;
 import notebook.model.repository.impl.UserRepository;
 import notebook.view.UserView;
@@ -12,8 +11,7 @@ import static notebook.util.DBConnector.createDB;
 public class Main {
     public static void main(String[] args) {
         createDB();
-        FileOperation fileOperation = new FileOperation(DB_PATH);
-        GBRepository repository = new UserRepository(fileOperation);
+        GBRepository repository = new UserRepository();
         UserController controller = new UserController(repository);
         UserView view = new UserView(controller);
         view.run();
--- src/notebook/model/repository/impl/UserRepository.java
+++ src/notebook/model/repository/impl/UserRepository.java
@@ -1,35 +1,43 @@
 package notebook.model.repository.impl;
 
-import notebook.model.dao.impl.FileOperation;
+
+import notebook.util.DBConnector;
+import notebook.util.UserValidator;
 import notebook.util.mapper.impl.UserMapper;
 import notebook.model.User;
 import notebook.model.repository.GBRepository;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class UserRepository implements GBRepository {
     private final UserMapper mapper;
-    private final FileOperation operation;
 
-    public UserRepository(FileOperation operation) {
+
+    public UserRepository() {
         this.mapper = new UserMapper();
-        this.operation = operation;
+
     }
 
     @Override
     public List<User> findAll() {
-        List<String> lines = operation.readAll();
+        List<String> lines = readAll();
         List<User> users = new ArrayList<>();
         for (String line : lines) {
             users.add(mapper.toOutput(line));
         }
         return users;
     }
-
+    public void userValid(User user){
+        UserValidator validator = new UserValidator();
+        user.setFirstName(validator.isNameValid(user.getFirstName()));
+        user.setLastName(validator.isNameValid(user.getLastName()));
+    }
     @Override
     public User create(User user) {
+        userValid(user);
         List<User> users = findAll();
         long max = 0L;
         for (User u : users) {
@@ -74,7 +82,46 @@ public class UserRepository implements GBRepository {
         for (User u: users) {
             lines.add(mapper.toInput(u));
         }
-        operation.saveAll(lines);
+        saveAll(lines);
+    }
+    public List<String> readAll() {
+        List<String> lines = new ArrayList<>();
+        try {
+            File file = new File(DBConnector.DB_PATH);
+            //создаем объект FileReader для объекта File
+            FileReader fr = new FileReader(file);
+            //создаем BufferedReader с существующего FileReader для построчного считывания
+            BufferedReader reader = new BufferedReader(fr);
+            // считаем сначала первую строку
+            String line = reader.readLine();
+            if (line != null) {
+                lines.add(line);
+            }
+            while (line != null) {
+                // считываем остальные строки в цикле
+                line = reader.readLine();
+                if (line != null) {
+                    lines.add(line);
+                }
+            }
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
     }
 
+    public void saveAll(List<String> data) {
+        try (FileWriter writer = new FileWriter(DBConnector.DB_PATH, false)) {
+            for (String line : data) {
+                // запись всей строки
+                writer.write(line);
+                // запись по символам
+                writer.append('\n');
+            }
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
